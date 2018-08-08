13264d1ed85a026596c00d2f929424fe010b23eb
2pLabIEM <filip.tomaska@gmail.com>
2018-08-08 03:08:25 +0200

    deletion of embarrasing code

--- MixVisualization.m
+++ MixVisualization.m
@@ -43,42 +43,16 @@ meanResp=male(:,n);
 spolu(:,n)=meanResp;
 end
 
-% % velke=mean(PSTH4Dall,1);
-% % velke=squeeze(velke);
-% % male=velke(1:25,:,:);
-% % male=mean(male,1);
-% % male=squeeze(male);
-% jedna=male(:,1);
-% dva=male(:,2);
-% tri=male(:,3);
-% styri=male(:,4);
-% pat=male(:,5);
-% sest=male(:,6);
-% sedem=male(:,7);
-% osem=male(:,8);
-% devat=male(:,9);
-% desat=male(:,10);
-% jedenast=male(:,11);
-% spolu(:,1)=jedna;
-% spolu(:,2)=dva;
-% spolu(:,3)=tri;
-% spolu(:,4)=styri;
-% spolu(:,5)=pat;
-% spolu(:,6)=sest;
-% spolu(:,7)=sedem;
-% spolu(:,8)=osem;
-% spolu(:,9)=devat;
-% spolu(:,10)=desat;
-% spolu(:,11)=jedenast;
+
 figure
 imagesc(spolu)
 end
 
 if rozhodnutie==2
-    
+
 prompt='vyber si cislo stimulu';
 cislo_stimulu=input(prompt);
-    
+
     listOfNeurons=dir('neuron*.mat');
 neuronsN=size(listOfNeurons,1);
 load(listOfNeurons(1,1).name, 'PSTHindividual');
@@ -101,7 +75,7 @@ for index=1:neuronsN
     load(listOfNeurons(index,1).name, 'PSTHindividual');
     PSTH4Dall(:,:,index,:)=PSTHindividual;
 end
-    
+
 velke=PSTH4Dall(:,:,:,cislo_stimulu);
 velke=squeeze(velke);
 male=velke(:,1:25,:);
@@ -110,7 +84,7 @@ male=squeeze(male);
 
 
 for n=1:neuronsN
-    
+
 meanResp=male(:,n);
 spolu(:,n)=meanResp;
 end
@@ -144,7 +118,7 @@ for index=1:neuronsN
     load(listOfNeurons(index,1).name, 'PSTHindividual');
     PSTH4Dall(:,:,index,:)=PSTHindividual;
 end
-   
+
 prompt='stimul 1';
 prvy=input(prompt);
 prompt='stimul 2';
